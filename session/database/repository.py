52ffc5e5276cec1bfa14816a075da2bd4fcf1f5c
eduardocostaam <eduardocosta942z@gmail.com>
2026-07-21 21:04:54 -0300

    feat: add endpoint to get messages from a session

--- session/database/repository.py
+++ session/database/repository.py
@@ -1,5 +1,5 @@
 from session.session import IRepository
-from session.entity import Session
+from session.entity import Session, Message
 from session.database import query as q
 
 class Repository(IRepository):
@@ -10,7 +10,7 @@ class Repository(IRepository):
         try:
             query, projection = q.get_user_sessions_query(id_user)
             sessions_data = self.db["session"].find(query, projection)
-            sessions = [from_data(data) for data in sessions_data]
+            sessions = [session_from_data(data) for data in sessions_data]
             if not sessions:
                 raise ValueError(f"No sessions found for user with id_user {id_user}.")
             return sessions
@@ -18,9 +18,32 @@ class Repository(IRepository):
             raise e
         except Exception as e:
             raise RuntimeError(f"Error fetching user sessions from database: {e}")
+        
+    def get_session_messages(self, id_session: str) -> list[Message]:
+        try:
+            query, projection = q.get_session_messages_query(id_session)
+            session_data = self.db["session"].find_one(query, projection)
+            if not session_data:
+                raise ValueError(f"No session found with id_session {id_session}.")
+            return [message_from_data(data) for data in session_data.get("messages", [])]
+        except ValueError as e:
+            raise e
+        except Exception as e:
+            raise RuntimeError(f"Error fetching session messages from database: {e}")
 
 
-def from_data(data: dict) -> Session:
+def message_from_data(data: dict) -> Message:
+    return Message(
+        input=data.get("input_message", data.get("input", "")),
+        output=data.get("output_message", data.get("output", data.get("ouput", ""))),
+        submitted_at=data["submitted_at"],
+        llm=data.get("llm", ""),
+        input_tokens=data.get("input_tokens", 0),
+        output_tokens=data.get("output_tokens", 0)
+    )
+
+
+def session_from_data(data: dict) -> Session:
     return Session(
         id=str(data["_id"]),
         id_user=str(data["id_user"]),
--- session/session.py
+++ session/session.py
@@ -1,12 +1,20 @@
 from abc import ABC, abstractmethod
-from session.entity import Session
+from session.entity import Session, Message
 
 class IRepository(ABC):
     @abstractmethod
     def getUserSessions(self, id_user: str) -> list[Session]:
         pass
 
+    @abstractmethod
+    def get_session_messages(self, id_session: str) -> list[Message]:
+        pass
+
 class IService(ABC):
     @abstractmethod
     def getUserSessions(self, id_external_user) -> list[Session]:
+        pass
+
+    @abstractmethod
+    def get_session_messages(self, id_session: str) -> list[Message]:
         pass
